22efb7833d14c399b02571a78b35d30bf299e5d2
Chris LeJeune <chrislejeune76@gmail.com>
2021-10-27 00:03:02 -0500

    improved functions

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
-buildFilmSelector = async (root) => {
+const createApplication = async ({ root }) => {
+  // const resFilms = await axios.get("https://swapi.dev/api/films/");
+  // const films = resFilms.data.results;
+  const films = await getFilms();
+
+  const filmOptions = [];
+  films.forEach((film, i) => {
+    filmOptions.push(`<option value="${i}">${film.title}</option>`);
+  });
+
+  const filmString = filmOptions.join("");
+
+  root.innerHTML = `
+    <select id="selectFilm">
+      ${filmString}
+    </select>
+  `;
+
+  //get planets for film 0
+  const planets = await getPlanets(films[0]);
+};
+
+const getFilms = async () => {
+  const resFilms = await axios.get("https://swapi.dev/api/films/");
+  return resFilms.data.results;
+};
+
+const getPlanets = async ({ planets }) => {
+  console.log(planets);
+  for (let planet of planets) {
+    const resPlanet = await axios.get(planet);
+    console.log(resPlanet.data);
+  }
+  // return an array of planet objects
+};
+
+buildApp = async (root) => {
   const resFilms = await axios.get("https://swapi.dev/api/films/");
   const films = resFilms.data.results;
 
@@ -24,5 +60,6 @@ buildFilmSelector = async (root) => {
     });
 };
 
-const filmSelector = document.querySelector("#filmPlace");
-buildFilmSelector(filmSelector);
+createApplication({
+  root: document.querySelector("#app"),
+});
